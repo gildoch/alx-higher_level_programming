4e7d87a5da836c0895557be1e831eb7d1240fa68
Gildo Chauze <gildochauze@gmail.com>
2024-01-18 15:09:32 +0200

    add save_to_file method to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,12 +33,34 @@ class Base:
 
         Returns:
         - str: JSON string representation of list_dictionaries.
-
-        Raises:
-        - TypeError: If list_dictionaries is not a list.
-        - ValueError: If any dictionary in the list is not serializable to JSON
         """
         # Ensure list_dictionary is a list
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Write the JSON string representation of list_objs to a file.
+
+        Parameters:
+        - cls (class): The class itself.
+        - list_objs (list): A list of instances that inherit from Base.
+
+        Returns:
+        - None
+
+        Raises:
+        - TypeError: If list_objs is not a list.
+        - ValueError: If any instance in the list does not inherit from Base.
+        """
+        filename = cls.__name__ + '.json'
+
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                json_string = cls.to_json_string(
+                    [obj.to_dictionary() for obj in list_objs])
+                f.write(json_string)
