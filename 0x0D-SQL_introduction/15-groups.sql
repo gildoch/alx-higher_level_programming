568d753523b292344de9b5b021cfca96ff6ef835
Gildo Chauze <gildochauze@gmail.com>
2024-02-13 11:50:21 +0200

    Mandatory tasks completed

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,1 @@
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- Lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command 
+
+USE hbtn_0c_0;
+SHOW TABLES;
