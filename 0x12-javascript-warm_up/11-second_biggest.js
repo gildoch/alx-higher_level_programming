e433317cdfa6bfa89f6fef33d26716f8e3193da9
Gildo Chauze <gildochauze@gmail.com>
2024-03-11 12:31:45 +0200

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const { argv } = require("process");
+let numbers;
+
+if (argv.length >= 3) {
+  numbers = argv.slice(2).map((number) => parseInt(number)).sort((a, b) => {
+    if (a < b) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log("0");
+} else {
+  console.log(
+    numbers[1]
+  );
+}
