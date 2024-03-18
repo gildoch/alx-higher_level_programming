534a85cbdccfcf0324ec7d48b04f0031b2e59d29
Gildo Chauze <gildochauze@gmail.com>
2024-03-18 21:01:08 +0200

    Fix: wrong output

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Mod
+script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
 """
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     """Query for all states on database"""
 
     engine = create_engine(
-        "mysql+pymysql://{}:{}@localhost:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost:33061/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True,
