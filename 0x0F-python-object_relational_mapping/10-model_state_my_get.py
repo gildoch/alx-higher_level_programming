3019946aafaa7b8b0b11242b0832ac9c2758346f
Gildo Chauze <gildochauze@gmail.com>
2024-03-19 16:06:41 +0200

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     """Query for all states on database"""
 
     engine = create_engine(
-        "mysql+pymysql://{}:{}@localhost:33061/{}".format(
+        "mysql+mysqldb://{}:{}@localhost:33061/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True,
