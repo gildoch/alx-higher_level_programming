02b8ad9c38ab32183e66b37415df6bc53478cab0
Gildo Chauze <gildochauze@gmail.com>
2024-07-30 20:45:16 +0200

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  body = JSON.parse(body);
+
+  const userTaskCount = {};
+  body.forEach(task => {
+    if (task.completed) {
+      if (userTaskCount[task.userId]) {
+        userTaskCount[task.userId]++;
+      } else {
+        userTaskCount[task.userId] = 1;
+      }
+    }
+  });
+
+  console.log(userTaskCount);
+});
